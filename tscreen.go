1bfeebfb5c2d435e238d56286be628355b59a0ee
Tulir Asokan <tulir@maunium.net>
2019-06-22 21:42:31 +0300

    Fix tmux check

--- tscreen.go
+++ tscreen.go
@@ -42,8 +42,8 @@ var escseq string
 // otherwise defaults taken from the terminal database are used.
 func NewTerminfoScreen() (Screen, error) {
 	term := os.Getenv("TERM")
-	if term == "screen" && len(os.Getenv("TMUX")) > 0 {
-		term = "tmux-256color"
+	if len(term) >= 6 && term[:6] == "screen" && len(os.Getenv("TMUX")) > 0 {
+		term = "tmux"
 	}
 	ti, e := terminfo.LookupTerminfo(os.Getenv("TERM"))
 	if e != nil {
